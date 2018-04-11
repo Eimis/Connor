70d6dd7cbeb9da739d684cc0b2d1c528669a9420
Eimantas <eimantas.stonys@gmail.com>
2018-04-11 15:51:15 +0200

    Added proper model instance display values

--- connor/connor/models.py
+++ connor/connor/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
+# from django.core.exceptions import ValidationError
 
 
+@python_2_unicode_compatible
 class WorkoutPlan(models.Model):
     name = models.CharField(max_length=30)
     user = models.ManyToManyField(User, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 # TODO: data migration
 # TODO: limit to 7
+@python_2_unicode_compatible
 class WeekDay(models.Model):
     DAYS_OF_WEEK = (
         (1, 'Monday'),
@@ -26,8 +33,15 @@ class WeekDay(models.Model):
         default=DAYS_OF_WEEK[0][0],
     )
 
+    def __str__(self):
+        return self.get_day_display()
 
+
+@python_2_unicode_compatible
 class WorkoutExcercise(models.Model):
     name = models.CharField(max_length=30)
     days = models.ManyToManyField(WeekDay, blank=True)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
