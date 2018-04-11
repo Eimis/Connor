1d62d0f2475a060094b14da234d3a237b6a20f11
Eimantas <eimantas.stonys@gmail.com>
2018-04-11 13:49:15 +0200

    Initial database schema for the app

---
+++ connor/connor/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class WorkoutPlan(models.Model):
+    name = models.CharField(max_length=30)
+    user = models.ManyToManyField(User, blank=True)
+
+
+# TODO: data migration
+# TODO: limit to 7
+class WeekDay(models.Model):
+    DAYS_OF_WEEK = (
+        (1, 'Monday'),
+        (2, 'Tuesday'),
+        (3, 'Wednesday'),
+        (4, 'Thursday'),
+        (5, 'Friday'),
+        (6, 'Saturday'),
+        (7, 'Sunday'),
+    )
+
+    day = models.CharField(
+        max_length=1,
+        choices=DAYS_OF_WEEK,
+        # 1st day of the week:
+        default=DAYS_OF_WEEK[0][0],
+    )
+
+
+class WorkoutExcercise(models.Model):
+    name = models.CharField(max_length=30)
+    days = models.ManyToManyField(WeekDay, blank=True)
+    description = models.TextField()
